513250407d68809fbf5312f87dc2e8c792c3cc1e
Weendx <muha10bin@list.ru>
2023-03-12 15:16:47 +0200

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,35 @@
 // Copyright 2021 NNTU-CS
 
+
+int fbinsearch(int const *arr, const int &size, const int &key) {
+    if (size == 0 || size == 1)
+        return -1;
+    if (key == arr[size / 2]) {
+        int cur = size / 2;
+        while (cur > -1 && arr[cur--] == key);
+        ++cur;
+        return cur;
+    }
+    if (key < arr[size / 2])
+        return fbinsearch(arr, size / 2, key);
+    int tmp = binsearch(arr + size / 2, size / 2 + size % 2, key);
+    if (tmp != -1) 
+        return tmp + size / 2;
+    else
+        return tmp;
+}
+
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int index = fbinsearch(arr, size, value);
+
+    if (index == -1)
+        return 0;
+
+    int valueCount = 0;
+    while (index < size) {
+        if (arr[index++] == value)
+            ++valueCount;
+    }
+
+    return valueCount; // если ничего не найдено
 }
